b46aec49b56a0308d18e2b97c0d24bb5689ca196
deadman96385 <seanhoyt963@gmail.com>
2015-08-04 23:41:20 -0400

    Slimify
    Change-Id: I3931e89876f806d8189f1622d1796bb618cfd990

--- slim.mk
+++ slim.mk
@@ -2,23 +2,23 @@
 TARGET_SCREEN_HEIGHT := 1080
 TARGET_SCREEN_WIDTH := 1920
 
-# Inherit some common CM stuff.
+# Inherit some common Slim stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/asus/Z00A/device.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/asus/Z00A/overlay
 
 PRODUCT_RUNTIMES := runtime_libart_default
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_Z00A
+PRODUCT_NAME := slim_Z00A
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_Z00A
 PRODUCT_MANUFACTURER := asus
